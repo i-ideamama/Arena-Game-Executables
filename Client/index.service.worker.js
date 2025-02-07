156210ea3d76b8d33e0fc05a06b405ee6d6e2f2e
Advaith Neelacantan <advaith.n@gmail.com>
2025-02-07 14:54:43 +0530

    a

--- Client/index.service.worker.js
+++ Client/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1738918630|17771928186';
+const CACHE_VERSION = '1738920278|19419871630';
 /** @type {string} */
 const CACHE_PREFIX = 'Player-Arena Gam-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
